46a6e68f899ffbe65e1259a81dd0f754f95e28e3
ReubenKabiti <reubenrk2@gmail.com>
2023-08-09 10:58:59 +0200

    restructure

--- maple_xml/std.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include <map>
-#include <string>
-#include <stack>
-#include <memory>
-
-using namespace std;
--- maple_xml/xml_parser.h
+++ maple_xml/xml_parser.h
@@ -1,16 +1,6 @@
 #pragma once
-#include "xml_tag.h"
+
 
 class XMLParser
 {
-public:
-	XMLParser(string text);
-	shared_ptr<XMLTag> Parse();
-private:
-	shared_ptr<XMLTag> GetRoot(shared_ptr<XMLTag> tag);
-	shared_ptr<XMLTag> ProcessTag(int i, int j);
-private:
-	/* parent stack used to store the current active parent on top */
-	stack<shared_ptr<XMLTag>> mParentStack;
-	std::string mText;
 };
